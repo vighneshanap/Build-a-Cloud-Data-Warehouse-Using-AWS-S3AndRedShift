22fcc9366fa25ade661081d35772043f5e613646
Vighnesh Anap <vighneshanapuk@gmail.com>
2020-07-10 18:09:24 +0100

    Update create_tables.py

--- create_tables.py
+++ create_tables.py
@@ -2,7 +2,7 @@ import configparser
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
-
+ 
 def drop_tables(cur, conn):
     """
      check the existing tables, drop if exists
